234fb86eb7e28c40a9b6db900ca572f11f24b0d7
Felipe V. Y. R. Pereira <felipe.croata@gmail.com>
2014-05-14 11:23:26 -0300

    install.sh: dependencies and other fixes

--- install.sh
+++ install.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
+
+set -e
+
 if [ "$(id -u)" != "0" ]; then
 	echo "Run this script with sudo."
 	exit 1
 fi
-apt-get install libevent-dev libboost1.46-dev libboost-dev zlibc zlib1g zlib1g-dev	
+
+echo "==== Dependencies ===="
+apt-get install gcc g++ make cmake automake autoconf libevent-dev libboost1.48-dev libboost-dev libssl-dev mono-devel pkg-config qt4-dev-tools zlibc zlib1g zlib1g-dev	
+
+echo "==== Cleaning ===="
 rm -rf include
 rm -rf lib
 mkdir -p include
 mkdir -p lib
+
+echo "==== gflags ===="
 cd gflags-2.1.1
-rm CMakeCache.txt
+rm -f CMakeCache.txt
 cmake .
-./configure
+./configure || echo "No configure"
 make
 cp -r include/* ../include
 cp lib/* ../lib
 cd ..
+
+echo "==== gtest ===="
 cd gtest-1.7.0
-rm CMakeCache.txt
+rm -f CMakeCache.txt
 cmake .
 ./configure
 make
 cp -r include/* ../include
 cp lib/* ../lib
-cp libgtest.a ../lib
+cp libgtest.a ../lib || cp lib/.libs/libgtest.a ../lib
 cd ..
+
+echo "==== thrift ===="
 cd thrift-0.9.1
+cp -f /usr/share/aclocal/pkg.m4 aclocal
+autoconf
+automake
 ./configure
 make
 make install
