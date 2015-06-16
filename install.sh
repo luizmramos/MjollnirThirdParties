5d0c1385cf0bfa7baee75d6b85d1b2055c722c71
Felipe V. Y. R. Pereira <felipe.croata@gmail.com>
2015-06-16 03:28:10 -0400

    gcc and g++ alternatives in update.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,13 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo "==== Dependencies ===="
-apt-get install gcc g++ make cmake automake autoconf bison flex libevent-dev libboost1.48-dev libssl-dev mono-devel mono-gmcs pkg-config qt4-dev-tools zlibc zlib1g zlib1g-dev
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-4.7 g++-4.7
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
+
+apt-get install make cmake automake autoconf bison flex libevent-dev libboost1.48-dev libssl-dev mono-devel mono-gmcs pkg-config qt4-dev-tools zlibc zlib1g zlib1g-dev
 
 echo "==== Cleaning ===="
 rm -rf include
